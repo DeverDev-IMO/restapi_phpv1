a57cbfb3c824aaad2340675245d06336677a223d
DeverDev-IMO <mking2499@hotmail.com>
2024-03-21 07:11:14 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Database Credentials
+ */
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'restapi_phpv1');
+
+/**
+ * Secret Key - JWT
+ */
+// define('SECRET_KEY', 'justjest');
+define('SECRET_KEY', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjFmZWEyNWMwLWU2YWMtMTFlZS1iYWZkLTA4YmZiOGJhMGVlYyJ9.NTFdjoT4pci8m0hrHPJ9NLgqcDlS-5pQw2onJEKIKVQ');
+
+/**
+ * Base URL
+ */
+// define('BASE_URL', 'http://127.0.0.1/github/myBooksAPI/backend/');
+define('BASE_URL', 'http://127.0.0.1/restapi_phpv1/');
+// define('BASE_URL', 'http://127.0.0.1/');
